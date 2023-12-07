712106f189f212994b0ee58e49928ea81ae5ee57
r2pen2 <joedobbelaar@gmail.com>
2023-12-07 17:45:43 -0500

    Somehow it took me 4 tries to get the anchor right???

--- client/src/routes/YogaBanner.jsx
+++ client/src/routes/YogaBanner.jsx
@@ -7,36 +7,36 @@ import glyph from "../assets/images/flower.png"
 export default function YogaBanner({userCanEditText}) {
   
   const Left = () => (
-    <section className="d-flex flex-column align-items-center justify-content-center py-2" style={{maxWidth: 650}}>
+    <div className="d-flex flex-column align-items-center justify-content-center py-2" style={{maxWidth: 650}}>
       <WLHeaderV2 h2 firestoreId="yoga-header" editable={userCanEditText} />
       <WLTextV2 firestoreId="yoga-body" editable={userCanEditText}>
         
       </WLTextV2>
-    </section>
+    </div>
   )
   
   const Right = () => (
-    <section className="d-flex flex-column align-items-center justify-content-center py-2" style={{maxWidth: 650}}>
+    <div className="d-flex flex-column align-items-center justify-content-center py-2" style={{maxWidth: 650}}>
       <WLHeaderV2 h2 firestoreId="gestalt-workshops-header" editable={userCanEditText}/>
       <WLTextV2 firestoreId="gestalt-workshops-body" editable={userCanEditText}/>
-    </section>
+    </div>
   )
 
   return (
     <section className="w-100 yoga-container d-flex flex-column align-items-center justify-content-center py-2" id="yoga-and-workshops" style={{zIndex: 2}}>
       <div className={`w-100`}>
-        <div className="wl-glyph-section-two-items-no-actions d-none d-lg-flex wl-gap-3">
+        <section className="wl-glyph-section-two-items-no-actions d-none d-lg-flex wl-gap-3">
           <Left />
           <div className="wl-glyph-section-two-items-no-actions-glyph-container">
             <img src={glyph} alt="glyph" className="wl-glyph-section-glyph" data-testid="wl-glyph-section-glyph" />
           </div>
           <Right />
-        </div>
+        </section>
         <div className="wl-glyph-section-two-items-no-actions d-flex d-lg-none">
           <div className="wl-glyph-section-two-items-no-actions-md-container container-fluid">
             <div className="row">
-              <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center" id="yoga-and-pilates"><Left /></div>
-              <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center" id="workshops"><Right /></div>
+              <section className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center" id="yoga-and-pilates"><Left /></section>
+              <section className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center" id="workshops"><Right /></section>
             </div>
           </div>
           <div className="wl-glyph-section-two-items-no-actions-glyph-container">
